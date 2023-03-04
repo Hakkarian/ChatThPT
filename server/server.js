3e0b335be5e83a2a6b19d9935c120293d13692c2
Hakkar <zugrutus@gmail.com>
2023-03-04 14:41:22 +0000

    deployattfour

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
 
-dotenv.config();
+dotenv.config({ path: "/etc/secrets/.env" });
 
-console.log(process.env.OPENAI_API_KEY)
 
 const configuration = new Configuration({
+    organization: "org-Dnot4KgfMsbGUqewnIhi0gMG",
     apiKey: process.env.OPENAI_API_KEY
 })
 
